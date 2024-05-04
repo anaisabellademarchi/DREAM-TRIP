28ce16dfacc4979aab66ffd3a0f89c79a051e9f3
Ana Isabella Demarchi Calegari <149118142+anaisabellademarchi@users.noreply.github.com>
2024-05-04 00:32:48 -0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\anais\\OneDrive\\Área de Trabalho\\Dream-Trip--main\\Dream-Trip--main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "PROJECT_DIR=C:\\Users\\anais\\OneDrive\\Área de Trabalho\\Dream-Trip--main\\Dream-Trip--main"
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\anais\\OneDrive\\Área de Trabalho\\Dream-Trip--main\\Dream-Trip--main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\anais\\OneDrive\\Área de Trabalho\\Dream-Trip--main\\Dream-Trip--main"
+  "FLUTTER_TARGET=C:\\Users\\anais\\OneDrive\\Área de Trabalho\\Dream-Trip--main\\Dream-Trip--main\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8wNDgxN2M5OWM5ZmQ0OTU2ZjI3NTA1MjA0ZjdlMzQ0MzM1ODEwYWVkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\anais\\OneDrive\\Área de Trabalho\\Dream-Trip--main\\Dream-Trip--main\\.dart_tool\\package_config.json"
+)
